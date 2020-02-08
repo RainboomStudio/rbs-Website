69286e326c93c5ac64dcd34f050e006d6ad9895f
Asthriona <im@asthriona.com>
2020-02-08 15:29:03 +0100

    Convert old website

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Rainboom Studio' });
 });
-
+router.get('/rainboom-engine', function(req, res, next) {
+  res.render('rbe', { title: 'Rainboom Engine' });
+});
 module.exports = router;
